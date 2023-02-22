3cf303fe6d36b8ee3c44368c246d9b2be87e3787
waqas1226 <112298023+waqas1226@users.noreply.github.com>
2023-02-22 15:56:12 +0500

    Delete backend directory

--- backend/models/Auth.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-const FireSchema = new Schema({
-    title:String,
-    desc:String,
-    urlType:String
-})
-module.exports=mongoose.model('FireFiles', FireSchema);
--- backend/verifyUser.js
+++
@@ -1,9 +0,0 @@
-
-// export const verifyUser=(req, res, next)=>{
-// // if(req.user.id===req.params.id || req.user.isAdmin){
-// //     next();
-// // }
-// //     else{
-//         console.log('sorry not authenticated')
-//     }
-
